acee0965fea725b624c681ef9128c326f4364a5f
vladyslav <fakingbrutal@gmail.com>
2021-10-22 13:59:05 +0300

    Commit7.
    В socket.js прописав const URL = process.env.REACT_APP_URL ||'localhost:5000';

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Chat from './src/components/chat/chat';
+import Chat from './components/chat/chat';
 
 ReactDOM.render(
   <React.StrictMode>
