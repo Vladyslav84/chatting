f2d25189e4210e64c71bd1ef8b7fcca8beb4c1b3
vladyslav <fakingbrutal@gmail.com>
2021-10-22 16:19:47 +0300

    https://server-for-chatting.herokuapp.com

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,5 +1,8 @@
-import {io} from 'socket.io-client';
-const URL = process.env.REACT_APP_URL ||'localhost:5000';
+import io from 'socket.io-client';
+require('dotenv').config()
+const URL = "https://server-for-chatting.herokuapp.com" || 'localhost:5000';
+// process.env.REACT_APP_URL = "https://server-for-chatting.herokuapp.com"
+console.log(process.env.REACT_APP_URL);
 
 export const socket = io(URL);
  
