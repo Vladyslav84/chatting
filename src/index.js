778bcb888b66884811f901554775fb69ec20b127
vladyslav <fakingbrutal@gmail.com>
2021-10-22 12:09:14 +0300

    Commit2.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Chat from '../src/components/chat/chat';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Chat />
   </React.StrictMode>,
   document.getElementById('root')
 );
