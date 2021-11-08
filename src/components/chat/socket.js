30d41102ac26882884859f883ccf30b73823049e
vladyslav <fakingbrutal@gmail.com>
2021-11-08 12:33:03 +0200

    Update socket.js
    || 'localhost:5000' добавив.

--- src/components/chat/socket.js
+++ src/components/chat/socket.js
@@ -1,7 +1,8 @@
 import io from 'socket.io-client';
 require('dotenv').config()
-const URL = "https://server-for-chatting.herokuapp.com" || 'localhost:5000';
-// process.env.REACT_APP_URL = "https://server-for-chatting.herokuapp.com"
+
+const URL = process.env.REACT_APP_URL || 'localhost:5000';
+
 console.log(process.env.REACT_APP_URL);
 
 export const socket = io(URL);
